e5db45b209e653a4287e95c930d82616f2ffc254
Lavanyavinjam5 <lavanyavinjam5@gmail.com>
2025-02-18 15:54:14 +0530

    Update PersistenceControoler.swift

--- PersistenceControoler.swift
+++ PersistenceControoler.swift
@@ -12,7 +12,7 @@ class PersistenceController {
     static let shared  = PersistenceController()
     let container: NSPersistentContainer
     private init() {
-        container = NSPersistentContainer(name: "Fitness")
+        container = NSPersistentContainer(name: "ELearningApp")
         container.loadPersistentStores { (storeDescription, error)  in
             if let error = error as NSError? {
                 fatalError ("unresolved  error \(error), \(error.userInfo)")
